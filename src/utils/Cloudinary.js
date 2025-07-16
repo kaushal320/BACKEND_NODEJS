d127b65a947d19e5949763075eb318a278612c5d
kaushal320 <nepalkaushal69@gmail.com>
2025-07-16 17:41:35 +0545

    made multer and cloudinary

---
+++ src/utils/Cloudinary.js
@@ -0,0 +1,28 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+// Configuration
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) {
+      throw new Error("Local file path is required for upload.");
+    }
+    //upload the file on cloudinary
+    const result = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto", // Automatically detect the resource type
+    });
+    console.log("File uploaded successfully:", result);
+    return result;
+  } catch (error) {
+    fs.unlinkSync(localFilePath);
+    console.error("Error uploading file:", error);
+  }
+};
+
+export { uploadOnCloudinary };
